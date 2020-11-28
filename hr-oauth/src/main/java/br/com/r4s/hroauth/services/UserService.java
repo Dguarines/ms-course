cc67d250f86fbc78fbf45e979a42cb3148d4a09a
Deivison <dguarines@gmail.com>
2020-11-27 22:39:40 -0300

    Login, Jwt token

--- hr-oauth/src/main/java/br/com/r4s/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/r4s/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package br.com.r4s.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.r4s.hroauth.entities.User;
 import br.com.r4s.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -25,6 +28,20 @@ public class UserService {
 		}
 		
 		logger.info("EMAIL found: " + email);
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("EMAIL not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		
+		logger.info("EMAIL found: " + username);
 		
 		return user;
 	}
